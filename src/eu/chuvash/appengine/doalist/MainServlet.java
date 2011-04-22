f90884abbd9f185328ecb85faf2e06d06879c7d8
Lusites <mirontoli@gmail.com>
2011-04-22 23:12:32 +0200

    now about page works

--- src/eu/chuvash/appengine/doalist/MainServlet.java
+++ src/eu/chuvash/appengine/doalist/MainServlet.java
@@ -1,13 +1,45 @@
 package eu.chuvash.appengine.doalist;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class MainServlet extends HttpServlet {
+	private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
+
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		doPost(req, resp);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		req.setAttribute("goesThroughMainServlet", new Boolean(true));
+		//this is in order to be able to track where requests come from
+		//all request which invoke jsp-files directly will be stripped.
+		//this will happen in include/validate.inc
+		//all other validating will happen here, in Controller role MainServlet
+		
+		resp.setContentType(CONTENT_TYPE);
+		String url = "/about.jsp";
+		String operation = req.getParameter("operation");
+		if (operation != null) {
+			if (operation.equals("about")) {
+				url = "/about.jsp";
+			}
+		}
+
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher(url);
+		try {
+			dispatcher.forward(req, resp);
+		} catch (ServletException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//resp.sendRedirect("/about.jsp");
 	}
 }
